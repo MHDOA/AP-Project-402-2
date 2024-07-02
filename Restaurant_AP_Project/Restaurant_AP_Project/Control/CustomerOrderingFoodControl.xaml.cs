06f85985590e4de4c00fbc71ad3445a4bbf17118
MHDOA <mohammadali4a@gmail.com>
2024-07-02 12:57:32 +0330

    Add Customer Ordering Fodd Control
    Have Grid and without any UI or UX

---
+++ Restaurant_AP_Project/Restaurant_AP_Project/Control/CustomerOrderingFoodControl.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Restaurant_AP_Project.Control
+{
+    /// <summary>
+    /// Interaction logic for CustomerOrderingFoodControl.xaml
+    /// </summary>
+    public partial class CustomerOrderingFoodControl : UserControl
+    {
+        public CustomerOrderingFoodControl()
+        {
+            InitializeComponent();
+        }
+
+        private void cmbCitiesClick(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show((sender as ComboBox).Text.ToString());
+        }
+    }
+}
