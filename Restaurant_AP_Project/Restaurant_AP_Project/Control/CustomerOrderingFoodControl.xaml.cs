4433eab244c5d340db4aaddf78fd82e26badd2be
MHDOA <mohammadali4a@gmail.com>
2024-07-03 15:28:45 +0330

    Add Customer Order Control
    Show for order and search in restaurants, have UI Only(Search Filter, Search Bar, ScrollViewer

--- Restaurant_AP_Project/Restaurant_AP_Project/Control/CustomerOrderingFoodControl.xaml.cs
+++ Restaurant_AP_Project/Restaurant_AP_Project/Control/CustomerOrderingFoodControl.xaml.cs
@@ -20,14 +20,82 @@ namespace Restaurant_AP_Project.Control
     /// </summary>
     public partial class CustomerOrderingFoodControl : UserControl
     {
+        bool IsDlivery, IsDine_In;
+        int rateFilter;
+        string cityFilter;
+
+        private void ScrollViewer_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            if (e.Delta > 0)
+                scrollViewer.LineUp();
+            else
+                scrollViewer.LineDown();
+
+            e.Handled = true;
+        }
+
         public CustomerOrderingFoodControl()
         {
             InitializeComponent();
+            IsDine_In = true;
+            IsDlivery = true;
+
+            int rateFilter = -1;
+
+            cityFilter = "";
         }
 
-        private void cmbCitiesClick(object sender, MouseButtonEventArgs e)
+        private void btnSearch(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show((sender as ComboBox).Text.ToString());
+
+        }
+
+        private void btnFilter(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(cityFilter + "City");
+            MessageBox.Show(rateFilter + "Rate");
+            MessageBox.Show(IsDlivery + "IsDlivery");
+            MessageBox.Show(IsDine_In + "IsDine_In");
+        }
+
+        private void cmbCitySelectionChange(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBoxItem cmbSelected = (sender as ComboBox).SelectedItem as ComboBoxItem;
+
+            if(cmbSelected.Name != "cmbAll")
+            {
+                cityFilter = cmbSelected.Content.ToString();
+            }
+            else
+            {
+                cityFilter = "";
+            }
+        }
+
+        private void chbUnCheck(object sender, RoutedEventArgs e)
+        {
+            CheckBox chb = sender as CheckBox;
+
+            if (chb.Tag.ToString() == "0") IsDine_In = false;
+            else if (chb.Tag.ToString() == "1") IsDlivery = false;
+        }
+
+        private void chbCheck(object sender, RoutedEventArgs e)
+        {
+            CheckBox chb = sender as CheckBox;
+
+            if (chb.Tag.ToString() == "0") IsDine_In = true;
+            else if (chb.Tag.ToString() == "1") IsDlivery = true;
+        }
+
+        private void URestaurantPreview_btnOrderClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void rbChecked(object sender, RoutedEventArgs e)
+        {
+            rateFilter = int.Parse((sender as RadioButton).Tag.ToString());
         }
     }
 }
