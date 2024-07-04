a82a081c0c06d0dae5b35f86254a38f0f3cd5022
AhmadMousavi1214 <ahmad1414141412@gmail.com>
2024-07-04 19:05:51 +0330

    stating tje admin panel

---
+++ Restaurant_AP_Project/Restaurant_AP_Project/Control/UInformationBox.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Restaurant_AP_Project.Control
+{
+    /// <summary>
+    /// Interaction logic for UInformationBox.xaml
+    /// </summary>
+    public partial class UInformationBox : UserControl
+    {
+
+        private int maxLength;
+        public int Max_Length
+        {
+            get { return maxLength; }
+            set { maxLength = value; }
+        }
+
+        private string blockText;
+        public string BlockText
+        {
+            get { return blockText; }
+            set { blockText = ":" + value ; }
+        }
+
+        private string boxText;
+        public string BoxText
+        {
+            get { return boxText; }
+            set { boxText = value; }
+        }
+
+        public UInformationBox()
+        {
+            maxLength = 25;
+            InitializeComponent();
+            DataContext = this;
+        }
+    }
+}
---
+++ Restaurant_AP_Project/Restaurant_AP_Project/Veiw/CustomerMainVeiw.xaml.cs
@@ -0,0 +1,54 @@
+﻿using Restaurant_AP_Project.Control;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Restaurant_AP_Project.Veiw
+{
+    /// <summary>
+    /// Interaction logic for CustomerMainVeiw.xaml
+    /// </summary>
+    public partial class CustomerMainVeiw : Window
+    {
+        public CustomerMainVeiw()
+        {
+            DataContext = this;
+            InitializeComponent();
+            MenuBox.SelectedItem = ProfileItem;
+            ContentVeiw.Children.Clear();
+            ContentVeiw.Children.Add(new CustomerProfileControl());
+        }
+
+        private void ProfileClick(object sender, MouseButtonEventArgs e)
+        {
+            ContentVeiw.Children.Clear();
+            ContentVeiw.Children.Add(new CustomerProfileControl());
+        }
+        private void OrderingFoodClick(object sender, MouseButtonEventArgs e)
+        {
+            ContentVeiw.Children.Clear();
+            ContentVeiw.Children.Add(new CustomerOrderingFoodControl());
+
+        }
+
+        private void HistoryOrderClick(object sender, MouseButtonEventArgs e)
+        {
+            ContentVeiw.Children.Clear();
+        }
+
+        private void SupportClick(object sender, MouseButtonEventArgs e)
+        {
+            ContentVeiw.Children.Clear();
+        }
+    }
+}
