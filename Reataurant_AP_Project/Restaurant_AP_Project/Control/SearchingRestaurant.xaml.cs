90a14444c68cbefd27e300f14ea64c9564cc9d5c
AhmadMousavi1214 <ahmad1414141412@gmail.com>
2024-07-05 01:21:10 +0330

    second user control for admin panel (which is for searching across restaurants) added

---
+++ Reataurant_AP_Project/Restaurant_AP_Project/Control/SearchingRestaurant.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Restaurant_AP_Project.Control
+{
+    /// <summary>
+    /// Interaction logic for SearchingRestaurant.xaml
+    /// </summary>
+    public partial class SearchingRestaurant : UserControl
+    {
+        public SearchingRestaurant()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
