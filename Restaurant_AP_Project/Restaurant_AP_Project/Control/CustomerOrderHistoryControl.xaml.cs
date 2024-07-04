5f6e6f59a8ff10c8887a8eca4f16669a2f533dc6
MHDOA <mohammadali4a@gmail.com>
2024-07-05 02:42:59 +0330

    Add Customer Order History
    Add history of orders for customer main view

---
+++ Restaurant_AP_Project/Restaurant_AP_Project/Control/CustomerOrderHistoryControl.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Restaurant_AP_Project.Control
+{
+    /// <summary>
+    /// Interaction logic for CustomerOrderHistoryControl.xaml
+    /// </summary>
+    public partial class CustomerOrderHistoryControl : UserControl
+    {
+        public CustomerOrderHistoryControl()
+        {
+            InitializeComponent();
+        }
+
+        private void btn(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
