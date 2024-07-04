59f0a812c70fcd35f8485b75bbb9a4ab205a209e
AhmadMousavi1214 <ahmad1414141412@gmail.com>
2024-07-05 01:45:41 +0330

    third user control for admin panel (which is for serching across complaints) added

---
+++ Reataurant_AP_Project/Restaurant_AP_Project/Control/SearchingComplaints.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Restaurant_AP_Project.Control
+{
+    /// <summary>
+    /// Interaction logic for SearchingComplaints.xaml
+    /// </summary>
+    public partial class SearchingComplaints : UserControl
+    {
+        public SearchingComplaints()
+        {
+            InitializeComponent();
+        }
+    }
+}
