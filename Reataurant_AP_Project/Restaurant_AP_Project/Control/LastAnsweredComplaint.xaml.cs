e4edbac261882eaa8753b591acafe1a3d922e473
AhmadMousavi1214 <ahmad1414141412@gmail.com>
2024-07-05 19:14:05 +0330

    the user control for watchign last answered complaints added

---
+++ Reataurant_AP_Project/Restaurant_AP_Project/Control/LastAnsweredComplaint.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Restaurant_AP_Project.Control
+{
+    /// <summary>
+    /// Interaction logic for LastAnsweredComplaint.xaml
+    /// </summary>
+    public partial class LastAnsweredComplaint : UserControl
+    {
+
+        public LastAnsweredComplaint()
+        {
+            InitializeComponent(); loadComplaints();
+        }
+        public void loadComplaints()
+        {
+            var complaints = new List<ComplaintControl>
+            {
+                new ComplaintControl { UserName = "این برای تست است", ComplaintTitle = "این برای تست است", Restaurant = "این برای تست است", Description ="تست تست تست تست تستتست تست تست تست تستتست تست تست تست تستتست تست تست تست تست", IsAnswered = true },
+                new ComplaintControl { UserName = "این برای تست است", ComplaintTitle = "این برای تست است", Restaurant = "این برای تست است", Description ="تست تست تست تست تستتست تست تست تست تستتست تست تست تست تستتست تست تست تست تست", IsAnswered = true },
+                new ComplaintControl { UserName = "این برای تست است", ComplaintTitle = "این برای تست است", Restaurant = "این برای تست است", Description ="تست تست تست تست تستتست تست تست تست تستتست تست تست تست تستتست تست تست تست تست", IsAnswered = true },
+                new ComplaintControl { UserName = "این برای تست است", ComplaintTitle = "این برای تست است", Restaurant = "این برای تست است", Description ="تست تست تست تست تستتست تست تست تست تستتست تست تست تست تستتست تست تست تست تست", IsAnswered = true },
+                new ComplaintControl { UserName = "این برای تست است", ComplaintTitle = "این برای تست است", Restaurant = "این برای تست است", Description ="تست تست تست تست تستتست تست تست تست تستتست تست تست تست تستتست تست تست تست تست", IsAnswered = true },
+                new ComplaintControl { UserName = "این برای تست است", ComplaintTitle = "این برای تست است", Restaurant = "این برای تست است", Description ="تست تست تست تست تستتست تست تست تست تستتست تست تست تست تستتست تست تست تست تست", IsAnswered = true },
+                new ComplaintControl { UserName = "این برای تست است", ComplaintTitle = "این برای تست است", Restaurant = "این برای تست است", Description ="تست تست تست تست تستتست تست تست تست تستتست تست تست تست تستتست تست تست تست تست", IsAnswered = true },
+                new ComplaintControl { UserName = "این برای تست است", ComplaintTitle = "این برای تست است", Restaurant = "این برای تست است", Description ="تست تست تست تست تستتست تست تست تست تستتست تست تست تست تستتست تست تست تست تست", IsAnswered = true },
+                new ComplaintControl { UserName = "این برای تست است", ComplaintTitle = "این برای تست است", Restaurant = "این برای تست است", Description ="تست تست تست تست تستتست تست تست تست تستتست تست تست تست تستتست تست تست تست تست", IsAnswered = true },
+                new ComplaintControl { UserName = "این برای تست است", ComplaintTitle = "این برای تست است", Restaurant = "این برای تست است", Description ="تست تست تست تست تستتست تست تست تست تستتست تست تست تست تستتست تست تست تست تست", IsAnswered = true },
+                new ComplaintControl { UserName = "این برای تست است", ComplaintTitle = "این برای تست است", Restaurant = "این برای تست است", Description ="تست تست تست تست تستتست تست تست تست تستتست تست تست تست تستتست تست تست تست تست", IsAnswered = true },
+                new ComplaintControl { UserName = "این برای تست است", ComplaintTitle = "این برای تست است", Restaurant = "این برای تست است", Description ="تست تست تست تست تستتست تست تست تست تستتست تست تست تست تستتست تست تست تست تست", IsAnswered = true },
+                new ComplaintControl { UserName = "این برای تست است", ComplaintTitle = "این برای تست است", Restaurant = "این برای تست است", Description ="تست تست تست تست تستتست تست تست تست تستتست تست تست تست تستتست تست تست تست تست", IsAnswered = true },
+                new ComplaintControl { UserName = "این برای تست است", ComplaintTitle = "این برای تست است", Restaurant = "این برای تست است", Description ="تست تست تست تست تستتست تست تست تست تستتست تست تست تست تستتست تست تست تست تست", IsAnswered = true },
+            };
+
+            lstComplaints.Items.Clear();
+            foreach (var complaint in complaints)
+            {
+                lstComplaints.Items.Add(complaint);
+            }
+        }
+    }
+}
