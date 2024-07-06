e182035128f8ceb87e1fddd9bcd997f3352a98f2
MHDOA <mohammadali4a@gmail.com>
2024-07-06 11:31:52 +0330

    Add Restaurant Main View
    Just have UI and categories

---
+++ Restaurant_AP_Project/Restaurant_AP_Project/Veiw/RestaurantMainView.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Restaurant_AP_Project.Veiw
+{
+    /// <summary>
+    /// Interaction logic for RestaurantMainView.xaml
+    /// </summary>
+    public partial class RestaurantMainView : Window
+    {
+        public RestaurantMainView()
+        {
+            InitializeComponent();
+        }
+
+        private void ChangeMenuItem(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void ChangeFoodInventory(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void EnableReserveService(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void OrderingReserve(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
