4b96962efeaa2104b762549ab1a4c90b62c67a0d
AhmadMousavi1214 <ahmad1414141412@gmail.com>
2024-07-07 00:20:10 +0330

    models folder with rwo model admin and complaint added to app

---
+++ Reataurant_AP_Project/Restaurant_AP_Project/Model/Complaint.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant_AP_Project.Model
+{
+    class Complaint
+    {
+        public int Id { get; set; }
+        public int RestaurantId { get; set; }
+        public string UsernameOfComplainer { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Restaurant { get; set; }
+        public string Answer { get; set; }
+        public bool IsAnswered { get; set; }
+    }
+}
