88c795d3742be03da262d552a7b8d22c514c7eb2
MHDOA <mohammadali4a@gmail.com>
2024-07-06 22:53:53 +0330

    Sign Up View Input Validity Added
    Add Input validiy and verify code to verify view, NEED send mail and load data

--- Restaurant_AP_Project/Restaurant_AP_Project/Veiw/SignUpVeiw.xaml.cs
+++ Restaurant_AP_Project/Restaurant_AP_Project/Veiw/SignUpVeiw.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Restaurant_AP_Project.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +20,22 @@ namespace Restaurant_AP_Project.Veiw
     /// </summary>
     public partial class SignUpVeiw : Window
     {
+        List<Customer> customers = new List<Customer>();
+
+        private void LoadData()
+        {
+
+        }
+
+        private void SendEmail(string str)
+        {
+
+        }
+
         public SignUpVeiw()
         {
             InitializeComponent();
+            LoadData();
         }
 
         private void MouseDownMove(object sender, MouseButtonEventArgs e)
@@ -34,9 +48,44 @@ namespace Restaurant_AP_Project.Veiw
 
         private void btnSignUp(object sender, RoutedEventArgs e)
         {
-           SignUpVerifyVeiw signUpVerifyVeiw = new SignUpVerifyVeiw();
-            signUpVerifyVeiw.Show();
-            Close();
+            try
+            {
+                // Input Validity
+                txtName.Text.IsCorrectName("نام");
+                txtLastName.Text.IsCorrectName("نام خانوادگی");
+                txtPhone.Text.IsCorrectPhone();
+                txtEmail.Text.IsCorrectEmail();
+                txtUsername.Text.IsCorrectUserName();
+
+                // Unique Username and Phone
+                var dupUserName = customers.Where(x => x.UserName == txtUsername.Text.Trim()).ToList();
+                if (dupUserName.Count > 0)
+                    throw new Exception("نام کاربری باید یکتا باشد");
+
+                var dupPhone = customers.Where(x => x.Phone == txtPhone.Text.Trim()).ToList();
+                if (dupPhone.Count > 0)
+                    throw new Exception("شماره همراه باید یکتا باشد");
+
+                // Create Verify Code
+                Random rand = new Random();
+                string verifyCode = rand.Next(1000, 9999).ToString();
+                SendEmail(verifyCode);
+
+                // Create Customer Id
+                int id;
+                do
+                {
+                    id = Guid.NewGuid().GetHashCode();
+                }while(id <= 0 || customers.Where(x => x.Id == id).Count() > 0);
+
+                SignUpVerifyVeiw signUpVerifyVeiw = new SignUpVerifyVeiw(id, txtName.Text.Trim(), txtLastName.Text.Trim(), txtPhone.Text.Trim(), txtEmail.Text.Trim(), txtUsername.Text.Trim(), verifyCode);
+                signUpVerifyVeiw.Show();
+                Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void btnClose(object sender, RoutedEventArgs e)
