c5557a9eae4638ff3a9b3fff156f5eea525da761
MHDOA <mohammadali4a@gmail.com>
2024-07-07 14:43:38 +0330

    Add Food Selecetion Control Loading Data
    Add loading data method to load food's and hshow them

--- Restaurant_AP_Project/Restaurant_AP_Project/Control/CustomerFoodSelectionControl.xaml.cs
+++ Restaurant_AP_Project/Restaurant_AP_Project/Control/CustomerFoodSelectionControl.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Restaurant_AP_Project.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,21 +21,62 @@ namespace Restaurant_AP_Project.Control
     /// </summary>
     public partial class CustomerFoodSelectionControl : UserControl
     {
-        public CustomerFoodSelectionControl()
+        List<Food> foods = new List<Food>();
+        Restaurant restaurant;
+
+        private void LoadFoods()
         {
+            foreach (var item in foods)
+            {
+                URestaurantFoodItemControl foodItem = new URestaurantFoodItemControl();
+                foodItem.FoodNameText = item.Name;
+                foodItem.FoodPrice = item.Price;
+                foodItem.FoodRate = item.Rate;
+                foodItem.FoodInventory = item.Inventory;
+                foodItem.FoodRawMaterial = item.RawMaterial;
+                try
+                {
+                    foodItem.FoodPictureAddress = (ImageSource)new ImageSourceConverter().ConvertFromString(item.ImageSource);
+                }
+                catch
+                {
+                    foodItem.FoodPictureAddress = (ImageSource)new ImageSourceConverter().ConvertFromString("../../../Asset/Element/DefaultPicture.png");
+                }
+                foodItem.btnOrderClick += btnOrderClick;
+
+                lstFoodItems.Items.Add(foodItem);
+            }
+        }
+
+        private void DataLoader(object sender, RoutedEventArgs e)
+        {
+            LoadFoods();
+        }
+
+        public CustomerFoodSelectionControl(List<Food> foods, Restaurant restaurant)
+        {
+            this.foods = foods;
+            this.restaurant = restaurant;
             InitializeComponent();
+            DataContext = this;
+            this.Loaded += DataLoader;
+            
         }
 
         private void btnBack(object sender, RoutedEventArgs e)
         {
             grdMain.Children.Clear();
-            grdMain.Children.Add(new CustomerOrderingFromRestaurantControl());
+            grdMain.Children.Add(new CustomerOrderingFromRestaurantControl(restaurant));
         }
 
         private void btnOrderClick(object sender, RoutedEventArgs e)
         {
+            URestaurantFoodItemControl foodItem = (URestaurantFoodItemControl)(((sender as Button).Parent as Grid).Parent as Border).Parent;
+
+            var food = foods.Where(x => x.Name == foodItem.FoodNameText).First();
+
             grdMain.Children.Clear();
-            grdMain.Children.Add(new CustomerFoodPageControl());
+            grdMain.Children.Add(new CustomerFoodPageControl(food, restaurant, foods));
         }
 
         private void URestaurantFoodItemControl_Loaded(object sender, RoutedEventArgs e)
